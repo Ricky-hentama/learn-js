e81d98098890d71d20f86daf9acf8d507cd68846
Ricky Hentama <rickyhendrapratama4@gmail.com>
2020-03-18 07:52:05 +0700

    update
    add reduce

--- Rest Parameter/r.js
+++ Rest Parameter/r.js
@@ -4,3 +4,9 @@ function nomor(a, ...args) {
   return args;
 }
 console.log(nomor(1, 2, 3, 4));
+
+//menjumlahkan semua parameter menggunakan rest parameter
+function jumlah(...no) {
+  return no.reduce((a, b) => a + b);
+}
+console.log(jumlah(10, 20, 30));
