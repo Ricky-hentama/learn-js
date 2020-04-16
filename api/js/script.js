2eceb5c327e54c745b8ab99a49f26e2123fd2a18
Ricky Hentama <rickyhendrapratama4@gmail.com>
2020-04-16 07:18:38 +0700

    update

--- api/js/script.js
+++ api/js/script.js
@@ -6,25 +6,38 @@ $.ajax({
     const film = data.Search;
     let card = "";
     film.forEach((e) => {
-      card += ` <div class="col-md-4 my-5">
-        <div class="card"">
-            <img src="${e.Poster}" class=" card-img-top" >
+      card += ` <div class="col-sm-4  my-5">
+        <div class="card">
+            <img src="${e.Poster}"  >
             <div class="card-body">
                 <h5 class="card-title">${e.Title}</h5>
                 <ul class="list-group list-group-flush">
-                <li class="list-group-item">Tipe : ${e.Type}</li>
                 <li class="list-group-item">Tahun : ${e.Year}</li>
                 
               </ul>
               
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="#"
+                data-toggle="modal"
+                data-imdbid="${e.imdbID}"
+                data-target="#exampleModal"  class="btn btn-primary detail-button">Detail</a>
             </div>
         </div>
     </div>`;
-      console.log(e);
     });
 
     $(".movie-card").html(card);
+
+    $(".detail-button").click(function (e) {
+      let imdb = e.target.dataset.imdbid;
+      $.ajax({
+        type: "post",
+        url: "http://www.omdbapi.com/?apikey=84244aeb&i=" + imdb,
+        data: "json",
+        success: function (detail) {
+          console.log(detail);
+        },
+      });
+    });
   },
   error: (e) => {
     console.log(e.responseText);
